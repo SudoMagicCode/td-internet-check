35ad2c0c69291874e8187205fe617ca51315bcc2
Matthew Ragan <raganmd@gmail.com>
2025-07-17 14:06:09 -0700

    Initial set-up for internet check

---
+++ TouchDesigner/td-python/internetStatus.py
@@ -0,0 +1,24 @@
+import socket
+
+
+class connection:
+
+    def __init__(self, ownerOp) -> None:
+        self.ownerOp = ownerOp
+        self._update_status(False)
+
+    def _update_status(self, status: bool) -> None:
+        self.ownerOp.par.Connected = status
+
+    def Check_status(self) -> None:
+        connection: bool = self._check_status(ip=self.ownerOp.par.Pollingip.eval(),
+                                              port=self.ownerOp.par.Pollingport.eval())
+        self._update_status(connection)
+
+    def _check_status(self, ip: str, port: int) -> bool:
+        a = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            a.connect((ip, port))
+            return True
+        except socket.error:
+            return False
